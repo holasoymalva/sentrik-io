fbe97df7af0efbb31cb20e0f92c1ccfec0a51083
Leon Martin <13058234+malvabombom@users.noreply.github.com>
2022-01-09 03:45:23 -0600

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,1 +1,17 @@
+import json
+import requests
 
+url = "https://fakestoreapi.com/products"
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    jsonData = json.dumps(obj, sort_keys=True, indent=4)
+    print(jsonData)
+    return jsonData
+
+response = requests.request("GET", url)
+
+if(response.status_code == 200):
+    jsonResponse = jprint(response.json())
+    with open('demo.json', 'w') as json_file:
+        json.dump(jsonResponse, json_file)
